638c2b54caa3bd2779d2feabad4f612f00b3a25d
Grzegorz Juszkiewicz <76259879+grjus@users.noreply.github.com>
2023-06-08 21:29:10 +0200

    dummy python

---
+++ src/main.py
@@ -0,0 +1,16 @@
+import os
+
+
+def write_string_to_file(file_name, string):
+    dir_path = '../resources'
+    if not os.path.exists(dir_path):
+        # create directory if it does not exist
+        os.makedirs(dir_path)
+
+    # create file path
+    file_path = os.path.join(dir_path, file_name)
+    with open(file_path, 'w') as f:
+        f.write(string)
+
+if __name__ == "__main__":
+    write_string_to_file('log.txt', 'Some message')
